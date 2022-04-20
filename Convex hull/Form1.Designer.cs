493c88e4cd7c5a996b76df6c5f908b069552cd59
Komisss <cool.blabla.cool@mail.ru>
2022-04-20 18:52:58 +0300

    Добавьте файлы проекта.

---
+++ Convex hull/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Convex_hull
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.AndrewAlgo = new System.Windows.Forms.Button();
+            this.ClearPlane = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(531, 435);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.zedGraphControl1_MouseDoubleClick);
+            // 
+            // AndrewAlgo
+            // 
+            this.AndrewAlgo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AndrewAlgo.Location = new System.Drawing.Point(12, 453);
+            this.AndrewAlgo.Name = "AndrewAlgo";
+            this.AndrewAlgo.Size = new System.Drawing.Size(108, 32);
+            this.AndrewAlgo.TabIndex = 1;
+            this.AndrewAlgo.Text = "Алгоритм Эндрю";
+            this.AndrewAlgo.UseVisualStyleBackColor = true;
+            this.AndrewAlgo.Click += new System.EventHandler(this.AndrewAlgo_Click);
+            // 
+            // ClearPlane
+            // 
+            this.ClearPlane.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ClearPlane.Location = new System.Drawing.Point(126, 453);
+            this.ClearPlane.Name = "ClearPlane";
+            this.ClearPlane.Size = new System.Drawing.Size(121, 32);
+            this.ClearPlane.TabIndex = 2;
+            this.ClearPlane.Text = "Очистить плоскость";
+            this.ClearPlane.UseVisualStyleBackColor = true;
+            this.ClearPlane.Click += new System.EventHandler(this.ClearPlane_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 487);
+            this.Controls.Add(this.ClearPlane);
+            this.Controls.Add(this.AndrewAlgo);
+            this.Controls.Add(this.zedGraphControl1);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button AndrewAlgo;
+        private System.Windows.Forms.Button ClearPlane;
+    }
+}
+
---
+++ Convex hull/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Convex_hull
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            draw();
+        }
+
+        //Здесь будут храниться все введенные точки.
+        List<Point> points = new List<Point> { };
+
+        List<Point> up = new List<Point> { };
+        List<Point> down = new List<Point> { };
+
+
+        public void draw()
+        {
+            GraphPane myPane = zedGraphControl1.GraphPane;
+
+            myPane.Title.Text = "Алгоритм Эндрю выпуклой оболочки";
+
+            //Делаю движение по графику при помощи ЛКМ
+            zedGraphControl1.PanButtons = MouseButtons.Left;
+            zedGraphControl1.PanModifierKeys = Keys.None;
+
+            //Очищаю массивы с точками для многоугольника, чтобы они не перечерчивались многократно.
+            myPane.CurveList.Clear();
+            myPane.GraphObjList.Clear();
+
+            //Выводятся введенные точки
+            PointPairList pointsList = new PointPairList();
+            for(int i = 0; i < points.Count; i++)
+            {
+                pointsList.Add(points[i].x, points[i].y);
+            }
+            LineItem myCurve = myPane.AddCurve("Scatter", pointsList, Color.Blue, SymbolType.Circle);
+            //Линии между точками не видны
+            myCurve.Line.IsVisible = false;
+            // Цвет заполнения отметок (кружков) - голубой
+            myCurve.Symbol.Fill.Color = Color.Blue;
+            // Тип заполнения - сплошная заливка
+            myCurve.Symbol.Fill.Type = FillType.Solid;
+
+            //Вывожу оболочку, которая расползяется вверх.
+            if (up.Count >= 2) 
+            {
+                //Выводится оболочка
+                for (int i = 0; i < up.Count - 1; i++)
+                {
+                    LineObj line = new LineObj(Color.Green, up[i].x, up[i].y, up[i + 1].x, up[i + 1].y);
+                    myPane.GraphObjList.Add(line);
+                }
+            }
+
+            //Вывожу оболочку, которая расползается вниз.
+            if (down.Count >= 2)
+            {
+                //Выводится оболочка
+                for (int i = 0; i < down.Count - 1; i++)
+                {
+                    LineObj line = new LineObj(Color.Green, down[i].x, down[i].y, down[i + 1].x, down[i + 1].y);
+                    myPane.GraphObjList.Add(line);
+                }
+            }
+
+            // Добавьте линии сетки на график и сделайте их серыми
+            myPane.XAxis.MajorGrid.IsVisible = true;
+            myPane.YAxis.MajorGrid.IsVisible = true;
+            myPane.XAxis.MajorGrid.Color = Color.Gray;
+            myPane.YAxis.MajorGrid.Color = Color.Gray;
+
+            // Ось X будет пересекаться с осью Y на уровне Y = 0
+            myPane.XAxis.Cross = 0.0;
+
+            // Ось Y будет пересекаться с осью X на уровне X = 0
+            myPane.YAxis.Cross = 0.0;
+
+            // Отключим отображение первых и последних меток по осям
+            myPane.XAxis.Scale.IsSkipFirstLabel = true;
+            myPane.XAxis.Scale.IsSkipLastLabel = true;
+
+            // Отключим отображение меток в точке пересечения с другой осью
+            myPane.XAxis.Scale.IsSkipCrossLabel = true;
+
+            // Спрячем заголовки осей
+            myPane.XAxis.Title.IsVisible = false;
+            myPane.YAxis.Title.IsVisible = false;
+
+            // Обновляем график
+            zedGraphControl1.Invalidate();
+        }
+
+        //Высчитываю, по часовой стрелке идут точки, или нет (алгоритм знаковой площади).
+        bool isClockWise(Point a, Point b, Point c)
+        {
+            return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) < 0;
+        }
+
+        //Высчитываю, против часовой стрелки идут точки, или по часовой (алгоритм знаковой площади).
+        bool isCounterClockWise (Point a, Point b, Point c)
+        {
+            return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) > 0;
+        }
+
+        private void zedGraphControl1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Сюда будут записаны координаты в системе координат графика.
+            double x, y;
+
+            // Пересчитываем пиксели в координаты на графике.
+            // У ZedGraph есть несколько перегруженных методов ReverseTransform.
+            zedGraphControl1.GraphPane.ReverseTransform(e.Location, out x, out y);
+
+            //Если человек ошибся, кликнув не на ту точку, на которую хотел, можно отменить, т.к. вылазиет
+            //окно с выбором.
+            DialogResult result = MessageBox.Show($"x = {x}, y = {y}", "Продолжить?", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                points.Add(new Point(x, y));
+                up.Clear();
+                down.Clear();
+                draw();
+            }
+        }
+
+
+        /*Алгоритм. Найдём самую левую и самую правую точки A и B 
+         * (если таких точек несколько, то возьмём самую нижнюю среди левых, и самую верхнюю среди правых).
+         * Понятно, что и A, и B обязательно попадут в выпуклую оболочку. Далее, проведём через них прямую AB,
+         * разделив множество всех точек на верхнее и нижнее подмножества S1 и S2 (точки, лежащие на прямой, 
+         * можно отнести к любому множеству - они всё равно не войдут в оболочку). Точки A и B отнесём к обоим множествам.
+         * Теперь построим для S1 верхнюю оболочку, а для S2 - нижнюю оболочку, и объединим их, получив ответ. 
+         * Чтобы получить, скажем, верхнюю оболочку, нужно отсортировать все точки по абсциссе, затем пройтись по всем точкам, 
+         * рассматривая на каждом шаге кроме самой точки две предыдущие точки, вошедшие в оболочку. Если текущая тройка
+         * точек образует не правый поворот (что легко проверить с помощью знаковой площади), то ближайшего соседа
+         * нужно удалить из оболочки. В конце концов, останутся только точки, входящие в выпуклую оболочку.
+         * 
+         * Итак, алгоритм заключается в сортировке всех точек по абсциссе и двух (в худшем случае) обходах всех точек, т.е. требуемая асимптотика O (N log N) достигнута.
+        */
+        private void AndrewAlgo_Click(object sender, EventArgs e)
+        {
+            //Если хочу повторить алгоритм на тех же точках, очищаю отрезки оболочки.
+            up.Clear();
+            down.Clear();
+            draw();
+
+
+            if(points.Count == 1)
+            {
+                MessageBox.Show("В массиве должно быть больше одной точки!");
+                return; 
+            }
+
+            //Сортирую точки от левой нижней до правой верхней.
+            points.Sort(new Point().Compare);
+
+            //Самая левая нижняя точка
+            Point p1 = points[0];
+
+            //Самая правая верхняя точка
+            Point p2 = points[points.Count - 1];
+
+            up.Add(p1);
+            down.Add(p1);
+
+
+            bool drawBySteps = false;
+
+            //Спрашиваю, отрисовывать по шагам оболочку или нет.
+            DialogResult drawByStepsLines = MessageBox.Show("Отрисовывать оболочку по шагам?", "", MessageBoxButtons.YesNo);
+            if (drawByStepsLines == DialogResult.Yes)
+            {
+                drawBySteps = true;
+            }
+
+            for (int i = 1; i < points.Count; i++)
+            {
+                if(i == points.Count - 1 || isClockWise(p1, points[i], p2))
+                {
+                    while (up.Count >= 2 &&  !isClockWise(up[up.Count - 2], up[up.Count - 1], points[i]))
+                    {
+                        up.RemoveAt(up.Count - 1);
+                    }
+                    up.Add(points[i]);
+                    if (drawBySteps)
+                    {
+                        draw();
+                        MessageBox.Show($"Шаг {i}");
+                    }
+                }
+                if(i == points.Count - 1 || isCounterClockWise(p1, points[i], p2))
+                {
+                    while (down.Count >= 2 && !isCounterClockWise(down[down.Count - 2], down[down.Count - 1], points[i]))
+                    {
+                        down.RemoveAt(down.Count - 1);
+                    }
+                    down.Add(points[i]);
+                    if (drawBySteps)
+                    {
+                        draw();
+                        MessageBox.Show($"Шаг {i}");
+                    }
+                }
+            }
+
+            //Отрисовываю выпуклую оболочку.
+            draw();
+        }
+
+        private void ClearPlane_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            up.Clear();
+            down.Clear();
+            draw();
+        }
+    }
+}
